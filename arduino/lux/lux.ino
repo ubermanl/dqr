a97fff605e2cf0c342df7aecd534cbde1b4e149a
Eric Nothen <enothen@gmail.com>
2017-05-28 01:07:42 -0300

    Actualizado completamente para poner los distintos tipos de dispositivo. Se agrego la cuenta para pasar de steps a Ampere.

--- arduino/lux/lux.ino
+++ arduino/lux/lux.ino
@@ -1,49 +1,134 @@
 /*
  * DqR Lux
  * Version: 1.0
- * --- Dqr Systems 2017 ---
+ * --- DqR Systems 2017 ---
  */
 
+/*
+ * This controls whether this DqR device will be lux, potentia, omni, or a combination of them.
+ * You need to enable or disable functions depending on the hardware configuration of the device
+ * where you will be uploading the code. Just switch between TRUE and FALSE as needed.
+ */
+boolean LUX = true;
+boolean POT = false;
+boolean OMNI = false;
 
-// Constants
-#define RELAY_OUTPUT 7
-#define TOUCH_INPUT  3
+/*
+ * These constants define cabling standards, so that all sensors are ALWAYS cabled in the same way
+ * accross the different arduino models. This tells you where each of the digital or analog signal
+ * cables go.
+ */
+#define POT_RELAY_OUT     2
+#define POT_AC_SENSOR_IN A2
+#define LUX_TOUCH_IN      3
+#define LUX_RELAY_OUT     4
+#define LUX_AC_SENSOR_IN A4
+/* TODO
+#define PIR_SENSOR_IN     
+#define SND_SENSOR_IN     
+#define TMP_SENSOR_IN     
+*/
 
-// Global Variables
-int relayState = HIGH;      // relay apagado
-int touchCurrent = LOW;
-int touchLastState = LOW;
+/*
+ * These global variables control device tuning
+ * 
+ */
+boolean luxTouchLast = false;
+boolean luxRelayState = true;
+int luxCurrentZero = 511;
 
-// Functions
-int toggleRelay(int relayState) {
-  if (relayState == HIGH) {
-    relayState = LOW;
-  } else {
-    relayState = HIGH;
+boolean potRelayState = true;
+int potCurrentZero = 511;
+
+unsigned long time = 0;
+
+
+/*
+ * Global function definitions: All funtions should go here, regardless of the DqR device they are meant for
+ * 
+ */
+
+// Update LUX relay state comparing last and current states of the touch sensor
+boolean setLuxRelay(boolean touchLast, boolean touchCurrent) {
+  if (touchCurrent && touchCurrent != touchLast ) {
+    luxRelayState = !luxRelayState;
+    digitalWrite(LUX_RELAY_OUT, luxRelayState);
   }
-  digitalWrite(RELAY_OUTPUT, relayState);
-  return relayState;
+  return touchCurrent;
 }
 
+/* 
+ *  This function reports the maximum value of a current sensor during a period of time.
+ *    sensor: the id of the sensor, could be the pin of lux or potentia
+ *    periodNumber: The number of 20ms periods that will be analized
+ *    iZero: The current zero of the sensor, which should be close to 511   
+ */
+int getCurrentValue(int sensor, int periodNumber, int iZero){
+  double startTime = millis();
+  int max = 0;
+  int rVal = 0;
+  while ((millis() - startTime) < 20*periodNumber) {
+    rVal = analogRead(sensor);
+    if (rVal > max) {
+      max = rVal;
+    }
+  }
+  return abs(max - iZero);
+}
 
-// Arduino Inicialization
+/*
+ * 
+ */
 void setup() {
-  pinMode(RELAY_OUTPUT, OUTPUT);
-  pinMode(TOUCH_INPUT, INPUT);
   Serial.begin(9600);
+
+  if ( LUX == true ) {
+    pinMode(LUX_TOUCH_IN, INPUT);
+    pinMode(LUX_RELAY_OUT, OUTPUT);
+    digitalWrite(LUX_RELAY_OUT, luxRelayState);
+    luxCurrentZero = getCurrentValue(LUX_AC_SENSOR_IN, 10, 0);
+  }
+  
+  if ( POT == true ) {
+    pinMode(POT_RELAY_OUT, OUTPUT);
+    digitalWrite(LUX_RELAY_OUT, potRelayState);
+    potCurrentZero = getCurrentValue(POT_AC_SENSOR_IN, 10, 0);
+  }
+  
+  if ( OMNI == true ) {
+  }
 }
 
-// Arduino operation
-void loop(){
-  
-  touchCurrent = digitalRead(TOUCH_INPUT);
 
-  if (touchCurrent == HIGH) {
-    if (touchLastState != touchCurrent) {
-      touchLastState = touchCurrent;
-      Serial.println("Touch presionado");
-      relayState = toggleRelay(relayState);     
-    }  
-  } else
-      touchLastState = LOW;
+/*
+ * Device's main loop
+ */
+void loop() {
+  // Timers are used so that data can be gathered at different intervals
+  time = millis();
+  unsigned int everySecond = time % 1000;
+  unsigned int everyFiveSeconds = time % 5000;
+  unsigned int everyTenSeconds = time % 10000;
+  unsigned int everyOneMinute = time % 60000;
+  unsigned int everyTenMinutes = time % 600000;
+  unsigned int everyOneHour = time % 3600000;
+  
+  // Collect AC data
+  if (everySecond == 0) {
+    double luxCurrentValue = getCurrentValue(LUX_AC_SENSOR_IN, 10, luxCurrentZero);
+    luxCurrentValue = luxCurrentValue * 50/1024;  // * 5/1024 to pass steps to volts, then *1000 to pass volts to milivolts, then /100 which is the sensor sensitivity
+    Serial.println(luxCurrentValue);
+  }
+
+  // Collect luminosity data
+  
+  // Collect sound sensor
+
+  // Collect presence data
+
+  // Collect temperature data
+
+  // Collect touch status and update Lux relay, if needed
+  luxTouchLast = setLuxRelay(luxTouchLast, digitalRead(LUX_TOUCH_IN));
+
 }
