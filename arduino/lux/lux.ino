4e267a3244137b0a15159eba3326d5a83a0064ae
maprea@openspirit.com.ar <maprea@openspirit.com.ar>
2017-06-05 20:14:07 -0300

    Lux: se incorpora mensajes serial

--- arduino/lux/lux.ino
+++ arduino/lux/lux.ino
@@ -6,13 +6,16 @@
 
 
 // Constants
-#define RELAY_OUTPUT 7
-#define TOUCH_INPUT  3
+#define LUX_OUTPUT 7
+#define TOUCH_INPUT  2
+#define CURRENT_INPUT A0
+#define LUX_DEFAULT_RELAY HIGH
 
 // Global Variables
-int relayState = HIGH;      // relay apagado
+int relayState = LUX_DEFAULT_RELAY;      // relay apagado
 int touchCurrent = LOW;
 int touchLastState = LOW;
+
 
 // Functions
 int toggleRelay(int relayState) {
@@ -21,23 +24,42 @@ int toggleRelay(int relayState) {
   } else {
     relayState = HIGH;
   }
-  digitalWrite(RELAY_OUTPUT, relayState);
+  digitalWrite(LUX_OUTPUT, relayState);
   return relayState;
+}
+
+void processMessages() {
+  if (Serial.available() > 0) {
+    Serial.print("Processing messages...");
+  	if (Serial.readString() == "R1") {
+    	relayState = toggleRelay(relayState);
+  	}
+    Serial.println(" Complete!");
+  }
 }
 
 
 // Arduino Inicialization
 void setup() {
-  pinMode(RELAY_OUTPUT, OUTPUT);
+  pinMode(LUX_OUTPUT, OUTPUT);
   pinMode(TOUCH_INPUT, INPUT);
   Serial.begin(9600);
+  Serial.flush();
+
+  digitalWrite(LUX_OUTPUT, LUX_DEFAULT_RELAY);
 }
 
 // Arduino operation
 void loop(){
-  
-  touchCurrent = digitalRead(TOUCH_INPUT);
 
+  // Process Messages
+  processMessages();
+  
+  // Gather sensor data
+  
+  
+  // Override Operation
+  touchCurrent = digitalRead(TOUCH_INPUT);
   if (touchCurrent == HIGH) {
     if (touchLastState != touchCurrent) {
       touchLastState = touchCurrent;
