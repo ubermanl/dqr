bc5b0bdd54273a35a11b96cff5ca0ccc759836cc
Eric Nothen <enothen@gmail.com>
2017-06-07 17:00:26 -0300

    Migrado todo el código a clases

--- arduino/dqr-device/dqr-device.h
+++ arduino/dqr-device/dqr-device.h
@@ -7,28 +7,153 @@
 #define DqR_h
 
 #include "Arduino.h"
+#include <BH1750.h>
 
-class Lux {
+// Generic class RelayDevice. Lux and Potentia classes inherite from this
+class RelayDevice {
   public:
-    Lux(int pinRelay, int pinTouch, int pinAcSensor, int acSensorSensitivity);
-    boolean setRelayStatus(boolean status);
+    RelayDevice(int, int, int);
+    boolean setRelayStatus(boolean);
     boolean getRelayStatus();
-  private:
-    boolean _status;
+    double getAcValue();
+  protected:
     int _pinRelay;
-    int _pinTouch;
     int _pinAcSensor;
-    int _acSensorSensitivity;
+    boolean _relayStatus;
+    int _acSensorSensitivity;  
+    int getCurrentSteps();
 };
 
-class Potentia {
-  public:
-    Potentia(int pin);
-    boolean setRelayStatus(boolean status);
-    boolean getRelayStatus();
-  private:
-    boolean _status;
-    int _pin;
+RelayDevice::RelayDevice(int pinRelay, int pinAcSensor, int acSensorSensitivity) {
+  _pinRelay = pinRelay;
+  _pinAcSensor = pinAcSensor;
+  _acSensorSensitivity = acSensorSensitivity;
 };
+
+boolean RelayDevice::setRelayStatus(boolean newStatus) {
+  if (_relayStatus != newStatus) {
+    digitalWrite(_pinRelay, newStatus);
+    _relayStatus = newStatus;
+  }
+  return _relayStatus;
+}
+
+boolean RelayDevice::getRelayStatus() {
+  return _relayStatus;
+}
+
+int RelayDevice::getCurrentSteps() {
+  int numberOfPeriods = 20; // We are measuring 20 cicles of a 50Hz function
+  double startTime = millis();
+  int max = 0;
+  int min = 1023;
+  int rVal = 0;
+  while ((millis() - startTime) < 20*numberOfPeriods) {
+    rVal = analogRead(_pinAcSensor);
+    if (rVal > max) {
+      max = rVal;
+    }
+    if (rVal < min) {
+      min = rVal;
+    }
+  }
+  return (max - min);
+}
+
+double RelayDevice::getAcValue() {
+  int steps = getCurrentSteps();
+  
+  // if steps <= 7, then it's probably noise. If it's not, then we can't measure this type of current anyway
+  if (steps <= 7) return 0;
+  
+  // The number of steps is multiplied by 5 and divided by 1024 to convert to volts, then divided by 2 to get
+  // just one side of the sin function.
+  double Vrms = ((steps * 5/1024) / 2);
+  
+  // The number of volts that we got above is multiplied by sqrt(2)/2 to get RMS
+  Vrms = Vrms * sqrt(2)/2;
+
+  // Finally, the actual AMPs result depends on the sensor sensitivity, which is in mV/A, so multiply by 1000
+  // to convert to milivolts and divide by sensitivity.
+  Vrms = Vrms * 1000/_acSensorSensitivity;
+
+  return Vrms;
+}
+
+
+// Class for Lux devices
+class Lux : public RelayDevice {
+  public:
+    Lux(int, int, int, int, int, int);
+    void setup();
+    int getSoundValue();
+    boolean getTouchStatus();
+    int getLumValue();
+    boolean getPirStatus();
+  private:
+    int _pinTouch;
+    boolean _touchLast = false;
+    int _pinSound;
+    int _pinPir;
+    BH1750 _lightSensor;
+};
+
+Lux::Lux(int pinRelay, int pinAcSensor, int acSensorSensitivity, int pinTouch, int pinSound, int pinPir): RelayDevice(pinRelay, pinAcSensor, acSensorSensitivity) {
+  _relayStatus = LOW; // LUX default is off
+  _pinTouch = pinTouch;
+  _pinSound = pinSound;
+  _pinPir = pinPir;  
+}
+
+void Lux::setup() {
+  pinMode(_pinRelay, OUTPUT);
+  pinMode(_pinAcSensor, INPUT);
+  pinMode(_pinTouch, INPUT);
+  pinMode(_pinSound, INPUT);
+  pinMode(_pinPir, INPUT);
+  digitalWrite(_pinRelay, _relayStatus);
+  digitalWrite(_pinPir, LOW); //Necesario? Probar comentado
+  _lightSensor.begin(BH1750_CONTINUOUS_HIGH_RES_MODE_2);
+};
+
+boolean Lux::getTouchStatus() {
+  boolean touchCurrent = digitalRead(_pinTouch);
+  if (touchCurrent != _touchLast) {
+    //Serial.println("*** touch pressed or released ***");
+    _touchLast = touchCurrent;
+    return ! _touchLast;
+  }
+  return LOW;
+}
+
+int Lux::getSoundValue() {
+  // falta suavizar
+  return analogRead(_pinSound);
+}
+
+boolean Lux::getPirStatus() {
+  return digitalRead(_pinPir);
+}
+
+int Lux::getLumValue() {
+  // falta suavizar
+  return _lightSensor.readLightLevel();
+}
+
+
+// Class for Potentia devices
+class Pot : public RelayDevice {
+  public:
+    Pot(int pinRelay, int pinAcSensor, int acSensorSensitivity);
+    void setup();
+};
+
+Pot::Pot(int pinRelay, int pinAcSensor, int acSensorSensitivity): RelayDevice(pinRelay, pinAcSensor, acSensorSensitivity) {
+};
+
+void Pot::setup() {
+  pinMode(_pinRelay, OUTPUT);
+  pinMode(_pinAcSensor, INPUT);
+}
 
 #endif
--- arduino/dqr-device/dqr-device.ino
+++ arduino/dqr-device/dqr-device.ino
@@ -10,15 +10,14 @@
  * where you will be uploading the code. Just switch between TRUE and FALSE as needed.
  */
 boolean LUX = true;
-boolean POT = false;
+boolean POT = true;
 boolean OMNI = false;
 
 /*
  * Libraries required by the different sensors
  */
-#include <Wire.h>
-#include <BH1750.h>
-//#include "dqr-device.h"
+//#include <Wire.h>  // not sure we are using this.
+#include "dqr-device.h"
 
 /*
  * These constants define cabling standards, so that all sensors are ALWAYS cabled in the same way
@@ -30,136 +29,37 @@ boolean OMNI = false;
 #define LUX_TOUCH_IN      3
 #define LUX_RELAY_OUT     4
 #define LUX_AC_SENSOR_IN A3
-#define LUX_LUM_SENS_SDA A4
-#define LUX_LUM_SENS_SCL A5
+#define LUX_LUM_SENS_SDA A4   # This one is a default, not passed to the Lux constructor
+#define LUX_LUM_SENS_SCL A5   # This one is a default, not passed to the Lux constructor
 #define LUX_PIR_SENS_IN   5
 #define LUX_SND_SENS_IN  A0
 #define LUX_PRESENCE_OUT 12
 /* TODO
-#define PIR_SENSOR_IN     
-#define SND_SENSOR_IN     
 #define TMP_SENSOR_IN     
 */
 
 /*
  * These global variables control device tuning
- * 
  */
-boolean luxTouchLast = false;
-boolean luxRelayState = true;
-
 int luxSensorSensitivity = 100;
-int luxNoiseSteps = 0;
-BH1750 lightSensor;
-
-boolean potRelayState = true;
-int potNoiseSteps = 0;
-
+int potSensorSensitivity = 100;
 unsigned long time = 0;
 
-
-/*
- * Global function definitions: All funtions should go here, regardless of the DqR device they are meant for
- * 
- */
-
-// Update LUX relay state comparing last and current states of the touch sensor
-boolean setLuxRelayState(boolean touchLast, boolean touchCurrent) {
-  if (touchCurrent && touchCurrent != touchLast ) {
-    luxRelayState = !luxRelayState;
-    digitalWrite(LUX_RELAY_OUT, luxRelayState);
-  }
-  return touchCurrent;
-}
-
-/* 
- *  This function reports the maximum value of a current sensor during a period of time.
- *    sensor: the id of the sensor, could be the pin of lux or potentia
- *    periodNumber: The number of 20ms periods that will be analized
- */
-int getCurrentValue(int sensor, int periodNumber){
-  double startTime = millis();
-  int max = 0;
-  int min = 1023;
-  int rVal = 0;
-  while ((millis() - startTime) < 20*periodNumber) {
-    rVal = analogRead(sensor);
-    if (rVal > max) {
-      max = rVal;
-    }
-    if (rVal < min) {
-      min = rVal;
-    }
-  }
-  return (max - min);
-}
-
-double currentStepsToAmps(double steps, int sensorSensitivity) {
-  // The number of steps is multiplied by 5 and divided by 1024 to convert to volts
-  // Then the number of volts is multiplied by 100 to convert to milivolts, and divided by the sensor sensitivity
-  if (steps <= 7) steps = 0;
-  double Vrms = ((steps * 5/1024) / 2) * sqrt(2)/2;
-  return Vrms * 1000/sensorSensitivity;
-  
-}
-
-double getSoundStatus() {
-  return analogRead(LUX_SND_SENS_IN);
-}
+Lux luxDevice(LUX_RELAY_OUT, LUX_AC_SENSOR_IN, luxSensorSensitivity, LUX_TOUCH_IN, LUX_SND_SENS_IN, LUX_PIR_SENS_IN);
+Pot potDevice(POT_RELAY_OUT, POT_AC_SENSOR_IN, potSensorSensitivity);
 
 
-boolean getPirStatus() {
-  return digitalRead(LUX_PIR_SENS_IN);
-  int startTime = millis();
-  while ((millis() - startTime < 100)) {
-    if (digitalRead(LUX_PIR_SENS_IN) == HIGH) {
-      return HIGH;
-    }
-  }
-  return LOW;
-}
-/*
- * 
- */
 void setup() {
+  if (LUX == true) luxDevice.setup();
+  if (POT == true) potDevice.setup();
+  
   Serial.begin(9600);
-  
-  if ( LUX == true ) {
-    pinMode(LUX_TOUCH_IN, INPUT);
-    pinMode(LUX_RELAY_OUT, OUTPUT);
-    pinMode(LUX_PIR_SENS_IN, INPUT);
-    pinMode(LUX_SND_SENS_IN, INPUT);
-    digitalWrite(LUX_PIR_SENS_IN, LOW);
-    pinMode(LUX_PRESENCE_OUT, OUTPUT);
-    
-    digitalWrite(LUX_RELAY_OUT, luxRelayState);
-    luxNoiseSteps = getCurrentValue(LUX_AC_SENSOR_IN, 10);
-    lightSensor.begin(BH1750_CONTINUOUS_HIGH_RES_MODE_2);
-  }
-  
-  if ( POT == true ) {
-    pinMode(POT_RELAY_OUT, OUTPUT);
-    digitalWrite(LUX_RELAY_OUT, potRelayState);
-    //potCurrentZero = getCurrentValue(POT_AC_SENSOR_IN, 10, 0);
-  }
-  
-  if ( OMNI == true ) {
-  }
-
   Serial.println("lum,snd,pir,luxamps,luxrelay");
 }
 
-
-/*
- * Device's main loop
- */
 void loop() {
-  double luxCurrentValue;
-  unsigned int luxLightValue;
-  bool pirStatus;
-  double sndStatus;
-  
   // Timers are used so that data can be gathered at different intervals
+  // TODO: replace with the timer library
   time = millis();
   unsigned int everySecond = time % 1000;
   unsigned int everyFiveSeconds = time % 5000;
@@ -167,54 +67,27 @@ void loop() {
   unsigned int everyOneMinute = time % 60000;
   unsigned int everyTenMinutes = time % 600000;
   unsigned int everyOneHour = time % 3600000;
+
+  // Override operation
+  if (luxDevice.getTouchStatus() == HIGH) luxDevice.setRelayStatus(!luxDevice.getRelayStatus());
   
-  // Collect AC data
-  if (everySecond == 0) {
-    luxCurrentValue = currentStepsToAmps(getCurrentValue(LUX_AC_SENSOR_IN, 10), luxSensorSensitivity);
+  if (everySecond == 0) {  
+    Serial.print(time);
+    Serial.print(": ");
+    Serial.print(luxDevice.getLumValue());
+    Serial.print(" | ");
+    Serial.print(luxDevice.getSoundValue());
+    Serial.print(" | ");
+    Serial.print(luxDevice.getPirStatus());
+    Serial.print(" | ");
+    Serial.print(luxDevice.getAcValue());
+    Serial.print(" | ");
+    Serial.print(luxDevice.getRelayStatus());
+    Serial.println("");
   }
-
-  // Collect luminosity data
-  if (everySecond == 0) {
-    luxLightValue = lightSensor.readLightLevel();
-  }
-
-  // Collect presence data
-  if (everySecond == 0) {
-    pirStatus = getPirStatus();
-  }
-
-  // Collect sound sensor
-  if (everySecond == 0) {
-    sndStatus = getSoundStatus();
-  }
-
-  // Collect temperature data
-
-  // Collect touch status and update Lux relay, if needed
-  luxTouchLast = setLuxRelayState(luxTouchLast, digitalRead(LUX_TOUCH_IN));
-  
-  // print overall status (send over network in the future)
   if (everyTenSeconds == 0) {
     Serial.println("");
     Serial.println("lum,snd,pir,luxamps,luxrelay");
   }
-  if (everySecond == 0) {
-    Serial.print(luxLightValue);
-    Serial.print(",");
-    Serial.print(sndStatus);
-    Serial.print(",");
-    Serial.print(pirStatus);
-    Serial.print(",");
-    Serial.print(luxCurrentValue);
-    Serial.print(",");
-    Serial.print(! luxRelayState);
-    Serial.println("");
-
-    if (pirStatus || sndStatus) {
-      digitalWrite(LUX_PRESENCE_OUT, HIGH);
-
-    } else {
-      digitalWrite(LUX_PRESENCE_OUT, LOW);
-    }
-  }
 }
+
